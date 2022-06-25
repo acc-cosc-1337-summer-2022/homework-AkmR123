2e4dd18eaa8a83d400037bad1ba406ae68a0dc74
AkmR123 <akm.raihanuzzaman@g.austincc.edu>
2022-06-25 20:00:41 +0000

    Enter

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,54 @@
 //add include statements
+#include "dna.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 //add function(s) code here
+
+double get_gc_content(const string& dna){
+    int GC = 0;
+    double size_of_dna = dna.size();
+
+    for (int i = 0; i != size_of_dna; i++) {
+        if (dna[i] == 'G' || dna[i] == 'C'){ 
+            GC++;
+        }
+    }
+    
+    double results = GC / size_of_dna;
+    return results;
+}
+
+string reverse_string(string dna){
+    string flipped;
+    for (int i = dna.size()-1; i >= 0; i--){
+        flipped += dna[i];
+    }
+    
+    return flipped;
+}
+
+string get_dna_complement(string dna) {
+    int size_of_dna = dna.size();
+    string reversed_dna = reverse_string(dna);
+
+    for (int i = 0; i != size_of_dna + 1; i++) {
+        if (reversed_dna[i] == 'A') {                
+            reversed_dna[i] = 'T';
+        } 
+        else if (reversed_dna[i] == 'T') {
+            reversed_dna[i] = 'A';
+        }
+        else if (reversed_dna[i] == 'G') {
+           reversed_dna[i] = 'C';
+        } 
+        else if (reversed_dna[i] == 'C') {
+            reversed_dna[i] = 'G';
+        }
+        
+    }
+
+    return reversed_dna;
+}
--- test/homework/04_repetition/04_iteration_tests.cpp
+++ test/homework/04_repetition/04_iteration_tests.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "dna.h"
 #include "catch.hpp"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+/* TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
-}
+} */
 
+TEST_CASE("Testing GC Content") {
+	REQUIRE(get_gc_content("AGCTATAG") == 0.375);
+	REQUIRE(get_gc_content("CGCTATAG") == 0.50);	
+}
+TEST_CASE("Testing reverse string") {
+	REQUIRE(reverse_string("AGCTATAG") == "GATATCGA");
+	REQUIRE(reverse_string("CGCTATAG") == "GATATCGC");	
+}
+TEST_CASE("Getting DNA Complement") {
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+}
