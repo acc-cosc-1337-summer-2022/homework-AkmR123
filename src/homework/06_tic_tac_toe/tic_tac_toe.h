14840b9e2936053dfa131d48c40f62fd8867d4f3
AkmR123 <akm.raihanuzzaman@g.austincc.edu>
2022-07-01 01:37:15 +0000

    Enter

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,26 @@
 //h
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player() const {return player;}
+    void display_board() const;
+
+private:
+    string player;
+    vector<string> pegs{9," "};
+    void clear_board();
+    void set_next_player();
+    bool check_board_full();
+};
+
+#endif
